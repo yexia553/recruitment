ce6acb6abdb0b610781876e3f02f32eb3ffdf3f3
yexia553 <1906390603@qq.com>
2020-12-10 23:24:54 +0800

    修改前段页面不能展示职位详情的bug，由于url配置最后没有加/导致

--- jobs/urls.py
+++ jobs/urls.py
@@ -15,7 +15,7 @@ from jobs import views
 urlpatterns = [
     url(r'^joblist/', views.job_list, name='job_list'),
     # url中使用括号来捕捉参数，通过?P<>来命名
-    url(r'job/(?P<job_id>\d+)$', views.job_detail, name='job_detail'),
+    url(r'job/(?P<job_id>\d+)/$', views.job_detail, name='job_detail'),
     path(r'resume/add/', views.ResumeCreateView.as_view(), name='resume_add'),
     path(r'resume/<int:pk>/', views.ResumeDetailView.as_view(), name='resume_detail'),
     # 设置首页，跳转到职位列表
